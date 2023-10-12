c5404a84294b3d6ae5524596c46bbe81e3e6832b
Sabir222 <mrsabir4@gmail.com>
2023-10-12 15:51:00 +0100

    Search

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -2,17 +2,18 @@
 import { BiSearch } from "react-icons/bi";
 const Search = () => {
   return (
-    <div className="border-[1px] md:w-full w-auto   rounded-lg transition max-w-[700px]  ">
-      <div className="flex flex-row  items-center justify-between">
-        <div className="text-sm font-semibold pl-5 pr-10 hover:bg-gray-100 text-gray-400 py-2 cursor-pointer   ease-in-out duration-150 rounded-r-lg w-full flex items-center relative justify-between md:w-[270px] lg:w-[500px]">
-          Search
-          <div className="p-2 ml-2 bg-black rounded-full absolute text-white flex  justify-between right-1">
-            <BiSearch size={10} />
-          </div>
-        </div>
+    <div className="flex justify-center items-center relative">
+      <input
+        type="text"
+        placeholder="Search"
+        className="text-sm font-semibold pl-5 pr-10 text-gray-400 py-2  ease-in-out duration-150 rounded-lg w-full flex items-center  justify-between md:w-[270px] lg:w-[500px] ring-1 ring-black hover:ring-2 "
+      />
+      <div className="p-2 ml-2 bg-black rounded-full absolute text-white flex  justify-between right-2">
+        <BiSearch size={10} />
       </div>
     </div>
   );
 };
 
 export default Search;
+
