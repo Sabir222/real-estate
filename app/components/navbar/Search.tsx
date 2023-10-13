493229aeb95c714b0aba25b9eb204b6ebe2aeb69
Sabir222 <mrsabir4@gmail.com>
2023-10-13 18:31:52 +0100

    Search

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -8,7 +8,10 @@ const Search = () => {
 
   const onSearch = (e: React.FormEvent) => {
     e.preventDefault();
-    const encodedSearchQuery = encodeURI(searchQuery);
+    if (searchQuery.trim() === "") {
+      return;
+    }
+    const encodedSearchQuery = encodeURI(searchQuery.trim());
     router.push(`/search?q=${encodedSearchQuery}`);
   };
 
@@ -22,7 +25,10 @@ const Search = () => {
           placeholder="Search for a city"
           className="text-sm font-semibold pl-5 pr-10 text-black py-2  ease-in-out duration-150 rounded-lg w-full flex items-center  justify-between md:w-[270px] lg:w-[500px] ring-1 ring-black hover:ring-2 "
         />
-        <div className="p-2 ml-2 bg-black rounded-full absolute text-white flex  justify-between right-2">
+        <div
+          className="p-2 ml-2 bg-black rounded-full absolute text-white flex  justify-between right-2 cursor-pointer"
+          onClick={onSearch}
+        >
           <BiSearch size={10} />
         </div>
       </div>
